c9545acf7e71322d18e1d92c583584534fe1fbca
Bruno Philippe <bruni.felipe@hotmail.com>
2019-08-12 00:28:46 -0300

    tindevfrontend

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import api from '../../services/api';
+
+import './Login.css';
+import logo from '../../assets/logo.svg';
+
+export default function Main({ history }) {
+  const [username, setUsername] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await api.post('devs', {
+      username
+    });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="tindev" />
+        <input
+          placeholder="Digite seu usuário do github"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Main/index.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { Link } from 'react-router-dom';
+import './main.css';
+
+import api from '../../services/api';
+
+import logo from '../../assets/logo.svg';
+import dislike from '../../assets/dislike.svg';
+import like from '../../assets/like.svg';
+import itsamatch from '../../assets/itsamatch.png';
+
+export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: { user: match.params.id }
+      });
+
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+      query: {
+        user: match.params.id
+      }
+    });
+
+    socket.on('match', dev => {
+      setMatchDev(dev);
+    })
+
+  }, [match.params.id])
+
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id }
+    });
+
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  return (
+    <div className="main-container">
+      <Link to="/">
+        <img src={logo} alt="Tindev" />
+      </Link>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt="logo" />
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+
+              <div className="buttons">
+                <button type="button">
+                  <img
+                    src={dislike}
+                    alt="Dislike"
+                    onClick={() => handleDislike(user._id)}
+                  />
+                </button>
+                <button type="button">
+                  <img
+                    src={like}
+                    alt="Like"
+                    onClick={() => handleLike(user._id)}
+                  />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty"> Acabou :( </div>
+      )}
+
+      { matchDev && (
+        <div className='match-container'>
+           <img src={itsamatch} alt="It's a match"></img>
+           <img className="avatar" alt="" src={matchDev.avatar} />
+           <strong>{matchDev.name}</strong>
+           <p>{matchDev.bio}</p>
+           <button type="button" onClick={() => setMatchDev(null)}>FECHAR</button>
+        </div>
+      )}
+    </div>
+  );
+}
